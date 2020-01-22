d16763c8f999cd76c99dd7d1610fd31945a76625
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-01-21 17:13:40 -0800

    my first commit

---
+++ data/cat-lifestyle.js
@@ -0,0 +1,63 @@
+
+const catBed = {
+    id: 'catBed',
+    name: 'Plush cat bed',
+    image: 'catBed.png',
+    description: 'Plush round pet bed sure to ease anxiety and offer warm and fluffly cuddles to your kitty.',
+    category: 'catAccessories',
+    price: 29,
+}
+
+const catHammock = {
+    id:
+    name:
+    image:
+    description:
+    category:
+    price: 
+}
+
+const catTree = {
+    id:
+    name:
+    image:
+    description:
+    category:
+    price: 
+}
+
+const catTunnel = {
+    id:
+    name:
+    image:
+    description:
+    category:
+    price: 
+}
+
+const catDazzler = {
+    id:
+    name:
+    image:
+    description:
+    category:
+    price: 
+}
+
+const catFuzz = {
+    id:
+    name:
+    image:
+    description:
+    category:
+    price: 
+}
+
+const catToy = {
+    id:
+    name:
+    image:
+    description:
+    category:
+    price: 
+}
